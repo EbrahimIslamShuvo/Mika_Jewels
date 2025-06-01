7fc31754ffcc53a5871c3d2d32d520b0c080e643
Weeny_Coder <ebrahimislam2003@gmail.com>
2025-06-02 03:05:49 +0600

    file setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,34 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
+import Root from './Root';
+import Home from './Pages/Home/Home';
+import Shop from './Pages/Shop/Shop';
+import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root> ,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/shop",
+        element: <Shop></Shop>
+      },
+      {
+        path: "/about",
+        element: <About></About>
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>
+      }
+    ]
   },
 ]);
 
