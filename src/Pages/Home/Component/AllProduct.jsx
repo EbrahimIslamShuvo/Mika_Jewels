9b26dcdfd913e7f0d4de41de6e55b8627f7a2d99
Weeny_Coder <ebrahimislam2003@gmail.com>
2025-06-11 14:15:27 +0600

    done without about

--- src/Pages/Home/Component/AllProduct.jsx
+++ src/Pages/Home/Component/AllProduct.jsx
@@ -18,8 +18,8 @@ export default function AllProduct() {
           disableOnInteraction: false,
         }}
         breakpoints={{
-          0: { slidesPerView: 1.5 },
-          640: { slidesPerView: 1.5 },
+          0: { slidesPerView: 1.1 },
+          640: { slidesPerView: 1.1 },
           768: { slidesPerView: 2 },
           1024: { slidesPerView: 5 },
         }}
