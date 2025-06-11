21e497ec6f89933baaac80eb0d1df6f2ae4b11e2
Weeny_Coder <ebrahimislam2003@gmail.com>
2025-06-11 14:18:36 +0600

    done without about

--- src/Pages/Home/Component/NewArrival.jsx
+++ src/Pages/Home/Component/NewArrival.jsx
@@ -29,8 +29,8 @@ export default function NewArrival() {
                 slidesPerView={1}
                 spaceBetween={10}
                 breakpoints={{
-                    640: { slidesPerView: 2.5, spaceBetween: 20 },
-                    768: { slidesPerView: 2.5, spaceBetween: 40 },
+                    640: { slidesPerView: 2, spaceBetween: 20 },
+                    768: { slidesPerView: 2, spaceBetween: 40 },
                     1024: { slidesPerView: 4, spaceBetween: 50 },
                 }}
                 modules={[Pagination]}
